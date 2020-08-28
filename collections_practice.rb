44b5a2236c701ac937d0b22f2f82b4cd085c0f3e
Trey Jennings <tjennings343@gmail.com>
2020-08-28 01:11:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr2)
+  arr2.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(arr3)
+  arr3.sort{|x,y| x.length <=> y.length}
+end
