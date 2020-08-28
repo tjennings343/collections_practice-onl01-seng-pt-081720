9a09449db63f97e577cbb382c0ea369e1d229248
Trey Jennings <tjennings343@gmail.com>
2020-08-28 01:25:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,6 +14,6 @@ end
 
 def swap_elements(arr4)
   arr4.each do |words|
-    arr4.sort_by {|words| [words[0], words[2], words[1]]}
+    arr4.sort_by {|words| [words[0], words[1], words[2]]}
   end
 end
